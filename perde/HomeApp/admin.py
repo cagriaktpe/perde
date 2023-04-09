04b3c28b94ce1341578298b8c3108841c7712711
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-04-10 01:22:23 +0200

    Start HomeApp, Add models to models.py, Register models to admin.py

---
+++ perde/HomeApp/admin.py
@@ -0,0 +1,49 @@
+from django.contrib import admin
+
+from .models import Movie, User, Rating, Watchlist, Comment, Genre, Director, Actor
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'year', 'rating')
+    list_filter = ('year', 'rating')
+    search_fields = ('title', 'year', 'rating')
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email')
+    search_fields = ('username', 'email')
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'movie', 'rating')
+    list_filter = ('user', 'movie')
+    search_fields = ('user', 'movie')
+
+@admin.register(Watchlist)
+class WatchlistAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+    list_filter = ('user',)
+    search_fields = ('user',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'movie', 'comment')
+    list_filter = ('user', 'movie')
+    search_fields = ('user', 'movie')
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Director)
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_day', 'country')
+    search_fields = ('name',)
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_day', 'country')
+    search_fields = ('name',)
+
+
