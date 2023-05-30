b55c351110851cf2f73767fd93057ea4fa2c132a
Erencan Ekinci <kyse1234@hotmail.com>
2023-05-30 16:00:19 +0200

    Add profile page

--- perde/perde/urls.py
+++ perde/perde/urls.py
@@ -30,4 +30,5 @@ urlpatterns = [
     path('add-comment/<str:movie_name>', HomeApp.views.add_comment, name='add-comment'),
     path('delete-comment/<str:movie_name>/', HomeApp.views.delete_comment, name='delete-comment'),
     path('aboutus/', HomeApp.views.aboutus, name='aboutus'),
+    path('profile/', HomeApp.views.profile, name='profile'),
 ]
